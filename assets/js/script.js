4e612db3879c19eef9ec11be2eaab22d5967c6b7
Depoule <jpoulet02100@gmail.com>
2019-02-04 14:14:30 +0100

    fini et corrigé

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// ici on applique une bordure pointillée verte à firsttext et thirdtext
+$(function(){
+  $('#firstText').css('border', '5px green dashed');
+  $('#thirdText').css('border', '5px green dashed');
+});
+// ou $('#firstText, #thirdText').css('border', '5px green dashed');
